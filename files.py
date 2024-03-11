4c68decb98fea0ad1364d45358d275e76416d5de
mkpoli <git@mkpo.li>
2024-03-11 15:48:01 +0900

    Add skin detection in files

--- files.py
+++ files.py
@@ -15,6 +15,35 @@ if not FTP_SERVER_MEDIAWIKI_DIR or not LOCAL_DIR or not TARGET_LANGUAGES:
     raise ValueError("Missing environment variables")
 
 
+def find_nested_files_by_metadata(
+    local_dir: Path, remote_dir: Path
+) -> dict[Path, Path]:
+    upload_map: dict[Path, Path] = {}
+
+    if local_dir.is_dir() and "metadata.yml" in [
+        file.name for file in local_dir.glob("*")
+    ]:
+        with open(local_dir / "metadata.yml", "r") as f:
+            metadata = yaml.safe_load(f)  # safer alternative with safe_load
+
+        for source_dir, target_dir in metadata["directories"].items():
+            source_path = local_dir / source_dir
+            target_path = remote_dir / target_dir
+
+            for lang in TARGET_LANGUAGES:
+                source_file = (source_path / f"{lang}.json").resolve()
+                if source_file.is_file():
+                    upload_map[source_file] = target_path / source_file.name
+
+        for source_file, target_file in metadata.get("files", {}).items():
+            source_path = (local_dir / source_file).resolve()
+            target_path = remote_dir / target_file
+
+            upload_map[source_path] = target_path
+
+    return upload_map
+
+
 def generate_upload_map(local_dir: str, remote_dir: str) -> dict[Path, Path]:
     upload_map: dict[Path, Path] = {}
 
@@ -43,31 +72,23 @@ def generate_upload_map(local_dir: str, remote_dir: str) -> dict[Path, Path]:
         file_path = file.resolve()
         upload_map[file_path] = remote_i18n_dir / file.name
 
+    # Extension translations
     local_extensions_dir = Path(local_dir, "extensions")
     remote_extensions_dir = Path(remote_dir, "extensions")
 
-    # Extension translations
     for extension_dir in local_extensions_dir.glob("*"):
-        if extension_dir.is_dir() and "metadata.yml" in [
-            file.name for file in extension_dir.glob("*")
-        ]:
-            with open(extension_dir / "metadata.yml", "r") as f:
-                metadata = yaml.safe_load(f)  # safer alternative with safe_load
+        upload_map |= find_nested_files_by_metadata(
+            extension_dir, remote_extensions_dir / extension_dir.name
+        )
 
-            for source_dir, target_dir in metadata["directories"].items():
-                source_path = extension_dir / source_dir
-                target_path = remote_extensions_dir / extension_dir.name / target_dir
+    # Skin translations
+    local_skins_dir = Path(local_dir, "skins")
+    remote_skins_dir = Path(remote_dir, "skins")
 
-                for lang in TARGET_LANGUAGES:
-                    source_file = (source_path / f"{lang}.json").resolve()
-                    if source_file.is_file():
-                        upload_map[source_file] = target_path / source_file.name
-
-            for source_file, target_file in metadata.get("files", {}).items():
-                source_path = (extension_dir / source_file).resolve()
-                target_path = remote_extensions_dir / extension_dir.name / target_file
-
-                upload_map[source_path] = target_path
+    for skin_dir in local_skins_dir.glob("*"):
+        upload_map |= find_nested_files_by_metadata(
+            skin_dir, remote_skins_dir / skin_dir.name
+        )
 
     # Upload Names.php
     local_names_php = Path(local_dir, "Names.php").resolve()
@@ -78,9 +99,18 @@ def generate_upload_map(local_dir: str, remote_dir: str) -> dict[Path, Path]:
 
 
 def main():
+    resolved_local_dir = Path(LOCAL_DIR).resolve()
+
+    print("LOCAL_BASE_DIR:", Path(LOCAL_DIR).absolute())
+    print("REMOTE_BASE_DIR:", FTP_SERVER_MEDIAWIKI_DIR)
+    print()
+
     file_map = generate_upload_map(LOCAL_DIR, FTP_SERVER_MEDIAWIKI_DIR)
     for local, remote in file_map.items():
-        print(f"{local}\n  -> {remote}")
+        relative_local_path = local.relative_to(resolved_local_dir)
+        relative_remote_path = remote.relative_to(FTP_SERVER_MEDIAWIKI_DIR)
+
+        print(f"{relative_local_path}\n  -> {relative_remote_path}")
 
 
 if __name__ == "__main__":
